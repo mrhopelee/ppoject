e4594e2ad2c6891b8eba7de049f223ebea288534
lihao <mrhopelee@gmail.com>
2015-08-12 21:00:02 +0800

    图片切换事件优化

--- javascript/index_photoshow.js
+++ javascript/index_photoshow.js
@@ -3,30 +3,77 @@ $(document).ready(	function() {
 	var btn_prev = $("#btn_prev"),
 		btn_next = $("#btn_next"),
 		body = $("body"),
-		photoshow = $("#photoshow").find("a"),
-		a, b,
-		temp0, temp1,
-		remp0,remp1,
-		img_num = 1,
-		img_str = " ",
-		rdiv = $(".photoshowbottom div"),
-		hiddenshadow= $(".hiddenshadow");
+		rdiv = $(".photoshowbottom div");
+	/*photoshow = $("#photoshow").find("a"),
+	 a, b,
+	 temp0, temp1,
+	 remp0,remp1,
+	 img_num = 1,
+	 img_str = " ",
+	 rdiv = $(".photoshowbottom div"),
+	 hiddenshadow= $(".hiddenshadow");*/
 
-
-	rdiv.click(function() {
-
-		a = img_num;
-		b = $(this).index()+1;
-		if(a==b){}else{
+	function ClickModule(){
+		var photoshow = $("#photoshow").find("a"),
+			a, b,
+			temp0, temp1,
+			remp0,remp1,
+			img_num = 1,
+			img_str = " ",
+			hiddenshadow= $(".hiddenshadow");
+		function rdivfun(thisnum) {
+			a = img_num;
+			b = thisnum+1;
+			if(a==b){}else{
+				hiddenshadowfun(hiddenshadow);
+				temp0 = photoshow.eq(img_num - 1);
+				temp1 = photoshow.eq(thisnum);
+				remp0 = rdiv.eq(img_num - 1);
+				remp1 = rdiv.eq(thisnum);
+				img_num=b;
+				remp0.css({
+					"border" : "4px solid #ccc",
+					"cursor" : "pointer"
+				});
+				remp1.css({
+					"border" : "4px solid #4C9ED9",
+					"cursor" : "unset"
+				});
+				photoshowInit(photoshow, a, b);
+				temp0.stop().fadeTo(800, 0, function() {
+					temp0.css({
+						"opacity" : "0",
+						"z-index" : "7",
+						"display" : "none"
+					});
+				});
+				temp1.stop().fadeTo(800, 1, function() {
+					temp1.css({
+						"z-index" : "8",
+						"display" : "block"
+					});
+				});
+			}
+		}
+		function prevfun() {/* img_num给图片编号，控制图片变化。 */
 			hiddenshadowfun(hiddenshadow);
-
-
-			temp0 = photoshow.eq(img_num - 1);
-			temp1 = photoshow.eq($(this).index());
-			remp0 = rdiv.eq(img_num - 1);
-			remp1 = rdiv.eq($(this).index());
-			img_num=b;
-
+			if (img_num == 1) {
+				img_num = 5;
+				a = 1;
+				b = 5;
+				temp0 = photoshow.eq(0);
+				temp1 = photoshow.eq(img_num - 1);
+				remp0 = rdiv.eq(0);
+				remp1 = rdiv.eq(img_num - 1);
+			} else {
+				img_num = img_num - 1;
+				a = img_num + 1;
+				b = img_num;
+				temp0 = photoshow.eq(img_num);
+				temp1 = photoshow.eq(img_num - 1);
+				remp0 = rdiv.eq(img_num);
+				remp1 = rdiv.eq(img_num - 1);
+			}
 			remp0.css({
 				"border" : "4px solid #ccc",
 				"cursor" : "pointer"
@@ -35,7 +82,6 @@ $(document).ready(	function() {
 				"border" : "4px solid #4C9ED9",
 				"cursor" : "unset"
 			});
-
 			photoshowInit(photoshow, a, b);
 			temp0.stop().fadeTo(800, 0, function() {
 				temp0.css({
@@ -50,101 +96,92 @@ $(document).ready(	function() {
 					"display" : "block"
 				});
 			});
+
+			img_str = "url(image/主页/bg" + img_num + ".jpg)";
 		}
-	});
+		function nextfun() {
+			hiddenshadowfun(hiddenshadow);
+			if (img_num == 5) {
+				img_num = 1;
+				a = 1;
+				b = 5;
+				temp0 = photoshow.eq(4);
+				temp1 = photoshow.eq(img_num - 1);
+				remp0 = rdiv.eq(4);
+				remp1 = rdiv.eq(img_num - 1);
+			} else {
+				img_num = img_num + 1;
+				a = img_num - 1;
+				b = img_num;
+				temp0 = photoshow.eq(img_num - 2);
+				temp1 = photoshow.eq(img_num - 1);
+				remp0 = rdiv.eq(img_num - 2);
+				remp1 = rdiv.eq(img_num - 1);
+			}
+
+			remp0.css({
+				"border" : "4px solid #ccc",
+				"cursor" : "pointer"
+			});
+			remp1.css({
+				"border" : "4px solid #4C9ED9",
+				"cursor" : "unset"
+			});
+			photoshowInit(photoshow, a, b);
+			temp0.stop().fadeTo(800, 0, function() {
+				temp0.css({
+					"opacity" : "0",
+					"z-index" : "7",
+					"display" : "none"
+				});
+			});
+			temp1.stop().fadeTo(800, 1, function() {
+				temp1.css({
+					"z-index" : "8",
+					"display" : "block"
+				});
+			});
+
+			img_str = "url(image/主页/bg" + img_num + ".jpg)";
+		}
+		return {
+			rdivfun: rdivfun,
+			prevfun: prevfun,
+			nextfun: nextfun
+		};
+	}
+	var t;
+	function startAutoNext(){
+		t = setInterval(
+			function(){
+				foo.nextfun();
+			},8000
+		);
+	}
+	function stopAutoNext(){
+		clearInterval(t);
+	}
+
 	/* 两个按钮的点击事件 */
-	btn_prev.click(function() {/* img_num给图片编号，控制图片变化。 */
-		hiddenshadowfun(hiddenshadow);
-		if (img_num == 1) {
-			img_num = 5;
-			a = 1;
-			b = 5;
-
-			temp0 = photoshow.eq(0);
-			temp1 = photoshow.eq(img_num - 1);
-			remp0 = rdiv.eq(0);
-			remp1 = rdiv.eq(img_num - 1);
-		} else {
-			img_num = img_num - 1;
-			a = img_num + 1;
-			b = img_num;
-
-			temp0 = photoshow.eq(img_num);
-			temp1 = photoshow.eq(img_num - 1);
-			remp0 = rdiv.eq(img_num);
-			remp1 = rdiv.eq(img_num - 1);
-		}
-		remp0.css({
-			"border" : "4px solid #ccc",
-			"cursor" : "pointer"
-		});
-		remp1.css({
-			"border" : "4px solid #4C9ED9",
-			"cursor" : "unset"
-		});
-		photoshowInit(photoshow, a, b);
-		temp0.stop().fadeTo(800, 0, function() {
-			temp0.css({
-				"opacity" : "0",
-				"z-index" : "7",
-				"display" : "none"
-			});
-		});
-		temp1.stop().fadeTo(800, 1, function() {
-			temp1.css({
-				"z-index" : "8",
-				"display" : "block"
-			});
-		});
-
-		img_str = "url(image/主页/bg" + img_num + ".jpg)";
+	var foo = ClickModule();
+	rdiv.click(function() {
+		stopAutoNext();
+		foo.rdivfun($(this).index());
+		startAutoNext();
 	});
-	btn_next.click(function autonext() {
-		hiddenshadowfun(hiddenshadow);
-		if (img_num == 5) {
-			img_num = 1;
-			a = 1;
-			b = 5;
-			temp0 = photoshow.eq(4);
-			temp1 = photoshow.eq(img_num - 1);
-			remp0 = rdiv.eq(4);
-			remp1 = rdiv.eq(img_num - 1);
-		} else {
-			img_num = img_num + 1;
-			a = img_num - 1;
-			b = img_num;
-			temp0 = photoshow.eq(img_num - 2);
-			temp1 = photoshow.eq(img_num - 1);
-			remp0 = rdiv.eq(img_num - 2);
-			remp1 = rdiv.eq(img_num - 1);
-		}
-
-		remp0.css({
-			"border" : "4px solid #ccc",
-			"cursor" : "pointer"
-		});
-		remp1.css({
-			"border" : "4px solid #4C9ED9",
-			"cursor" : "unset"
-		});
-		photoshowInit(photoshow, a, b);
-		temp0.stop().fadeTo(800, 0, function() {
-			temp0.css({
-				"opacity" : "0",
-				"z-index" : "7",
-				"display" : "none"
-			});
-		});
-		temp1.stop().fadeTo(800, 1, function() {
-			temp1.css({
-				"z-index" : "8",
-				"display" : "block"
-			});
-		});
-
-		img_str = "url(image/主页/bg" + img_num + ".jpg)";
+	btn_prev.click(function(){
+		stopAutoNext();
+		foo.prevfun();
+		startAutoNext();
 	});
-	setTimeout(alert("123"),"50000");
+	btn_next.click(function(){
+		stopAutoNext();
+		foo.nextfun();
+		startAutoNext();
+	});
+	startAutoNext();
+	/*setInterval("autonex()",5000)
+	 function autonex(){foo.nextfun();}*/
 });
 function hiddenshadowfun(hiddenshadow){
 	hiddenshadow.css({
